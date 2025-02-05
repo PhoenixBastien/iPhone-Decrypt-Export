f21562083613902398d659807b581f879c06c11f
Phoenix Bastien <phoenixbastien@gmail.com>
2025-02-05 13:10:03 -0500

    added whatsapp extract and errors printed

--- decrypt.py
+++ decrypt.py
@@ -1,27 +1,30 @@
 from iphone_backup_decrypt import (
-    EncryptedBackup, RelativePath, RelativePathsLike
+    EncryptedBackup, RelativePath, RelativePathsLike, MatchFiles
 )
 
 def main():
     print('Decrypting backup...')
     try:
-        passphrase = 'Fall2024!'
+        backup_code = 'Canada!1'
         backup_path = './backup'
-        backup = EncryptedBackup(
-            backup_directory=backup_path, passphrase=passphrase
-        )
+        backup = EncryptedBackup(backup_directory=backup_path,
+                                 passphrase=backup_code)
+
         # Extract the SMS SQLite database and attachments
-        backup.extract_file(
-            relative_path=RelativePath.TEXT_MESSAGES,
-            output_filename='./Library/SMS/sms.db'
-        )
-        backup.extract_files(
-            relative_paths_like=RelativePathsLike.SMS_ATTACHMENTS,
-            output_folder='.', preserve_folders=True
-        )
+        backup.extract_file(relative_path=RelativePath.TEXT_MESSAGES,
+                            output_filename='./Library/SMS/sms.db')
+        backup.extract_files(relative_paths_like=RelativePathsLike.SMS_ATTACHMENTS,
+                             output_folder='.', preserve_folders=True)
+
+        # Extract WhatsApp SQLite database and attachments:
+        backup.extract_file(relative_path=RelativePath.WHATSAPP_MESSAGES,
+                            output_filename="./output/whatsapp.sqlite")
+        backup.extract_files(**MatchFiles.WHATSAPP_ATTACHMENTS,
+                            output_folder="./output/whatsapp",
+                            preserve_folders=False)
         print('Decryption successful!')
-    except:
-        print('Decryption failed!')
+    except Exception as error:
+        print('Decryption failed!', error)
 
 if __name__ == '__main__':
     main()
