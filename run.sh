4a286733b8f790fec1e129ba4e7037124716121e
Phoenix Bastien <phoenixbastien@gmail.com>
2025-02-05 13:08:28 -0500

    code recognizes if user is using wsl, username passed as build arg, and other changes

--- run.sh
+++ run.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 
-# set device hash string and set backup path from device hash string
-DEVICE_HASH=00008030-001C050E0EBB802E
-BACKUP_PATH="$HOME/Library/Application Support/MobileSync/Backup/$DEVICE_HASH"
+# set device hash string, local backup path and local export path
+DEVICE_HASH=00008101-000D49980121001E
+# USERNAME=$(cmd.exe /c echo %USERNAME%)
+# USERPROFILE=$(wslpath "C:/Users/$USERNAME")
+
+if [ -f "/proc/sys/fs/binfmt_misc/WSLInterop" ]
+then # Windows
+    echo "User is running WSL through Windows."
+    # convert windows paths to wsl paths
+    USERNAME=PS_ENG
+    LOCAL_BACKUP_PATH=$(wslpath "C:/Users/$USERNAME/AppData/Roaming/Apple Computer/MobileSync/Backup/$DEVICE_HASH")
+    LOCAL_EXPORT_PATH=$(wslpath "C:/Users/$USERNAME/Downloads/export")
+else # Mac
+    echo "User is a true Unix enjoyer."
+    LOCAL_BACKUP_PATH="$HOME/Library/Application Support/MobileSync/Backup/$DEVICE_HASH"
+    LOCAL_EXPORT_PATH="$HOME/Downloads/export"
+fi
+#TODO
+# stat $LOCAL_BACKUP_PATH -c "%n,%.19y" | sed '1i Device Hash,Modified Date\n' | column -s, -t
+# su -
+# mkdir -p /mnt/d
+# sudo mount -t drvfs D: /mnt/d
+# LOCAL_EXPORT_PATH=/mnt/d/export # usb key
+#####
+rm -rf $LOCAL_EXPORT_PATH
 # pull python docker image
 docker pull python:latest
 # stop all running containers and force remove all unused containers
-docker stop $(docker ps -q)
+# docker stop $(docker ps -q)
 docker container prune -f
-# remove export dir
-rm -rf export
 # build image and run new container from image
-docker build --build-arg DEVICE_HASH=$DEVICE_HASH -t docker-image:test .
-docker run --rm -v ./bin:/home/phoenix/bin \
-                -v "$BACKUP_PATH":/home/phoenix/backup \
-                -v ./export:/home/phoenix/export \
-                docker-image:test
+docker build --build-arg USER=$USER -t imessage-image:test .
+docker run --rm -it --name imessage-container \
+    -v ./bin:/home/$USER/bin \
+    -v "$LOCAL_BACKUP_PATH":/home/$USER/backup \
+    -v "$LOCAL_EXPORT_PATH":/home/$USER/export \
+    imessage-image:test
