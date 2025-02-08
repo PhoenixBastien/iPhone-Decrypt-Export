c1b4d4b4055c65459f949952b0b0a696c6407371
Phoenix Bastien <phoenixbastien@gmail.com>
2025-02-07 19:09:21 -0500

    code moved to Dockerfile and python script

--- main.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# add bin dir to PATH
-PATH=~/bin:$PATH
-# run python decrypt script
-python -u decrypt.py
-# export messages to html
-imessage-exporter -f html -c full -p ~/Library/SMS/sms.db -o ~/export
