c4108766242c43ac82c5548ec9972968f69c983e
Phoenix Bastien <phoenixbastien@gmail.com>
2025-02-13 14:29:13 -0500

    fixed path overwriting bug on windows and simplified code

--- run.sh
+++ run.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
 # check for wsl interoperation file (only present in windows)
-if [ -f "/proc/sys/fs/binfmt_misc/WSLInterop" ]
-then # User is running WSL through Windows (pin of shame)
-    # get windows env variables with cmd.exe and convert paths to wsl paths
-    APPDATA=$(wslpath $(cmd.exe /c echo %APPDATA%))
-    USERPROFILE=$(wslpath $(cmd.exe /c echo %USERPROFILE%))
-    BACKUP_ROOT="$APPDATA/Apple Computer/MobileSync/Backup"
-    EXPORT_ROOT="$USERPROFILE/Export"
-else # User is a true Unix enjoyer (Mac or Linux)
+if [ ! -f "/proc/sys/fs/binfmt_misc/WSLInterop" ]
+then # user is a true unix enjoyer (mac or linux)
+    # get local backup root path
     BACKUP_ROOT="$HOME/Library/Application Support/MobileSync/Backup"
-    EXPORT_ROOT="$HOME/Export"
+else # user is running wsl through windows (pin of shame)
+    # get windows home dir with powershell and convert to wsl path without '\r'
+    HOME=$(wslpath $(powershell.exe '$HOME') | tr -d '\r')
+    # get local backup root path
+    BACKUP_ROOT="$HOME/AppData/Roaming/Apple Computer/MobileSync/Backup"
 fi
 
-# remove previous export
-rm -rf $EXPORT_ROOT # for testing but should be removed
+# get local export root path
+EXPORT_ROOT="$HOME/Export"
+# remove previous export (for testing but should be removed)
+rm -rf $EXPORT_ROOT
+
 # pull docker images
 docker pull python:alpine
 docker pull rust:alpine
-# build image and run new container from image
+
+# build image and run container with mounted backup and export volumes
 docker build -t imessage-decrypt-export:latest .
 docker run --rm -it -v "$BACKUP_ROOT":/mnt/Backup -v "$EXPORT_ROOT":/mnt/Export \
     imessage-decrypt-export:latest
