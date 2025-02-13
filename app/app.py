42bb80066c654764bb770c722e70425e5bb7c50b
Phoenix Bastien <phoenixbastien@gmail.com>
2025-02-13 11:19:36 -0500

    minor changes to app script

--- app/app.py
+++ app/app.py
@@ -4,14 +4,16 @@ from iphone_backup_decrypt import (
 import subprocess
 import os
 import plistlib
-import pandas as pd
+# import pandas as pd
 
 def extract_imessage(backup: EncryptedBackup) -> None:
     '''Extract iMessage database and attachments from backup.'''
     HOME = os.getenv('HOME')
-    backup.extract_file(relative_path=RelativePath.TEXT_MESSAGES,
-                        output_filename=f'{HOME}/Library/SMS/sms.db')
-    backup.extract_files(relative_paths_like=RelativePathsLike.SMS_ATTACHMENTS,
+    # backup.extract_file(relative_path=RelativePath.TEXT_MESSAGES,
+    #                     output_filename=f'{HOME}/Library/SMS/sms.db')
+    # backup.extract_files(relative_paths_like=RelativePathsLike.SMS_ATTACHMENTS,
+    #                      output_folder=HOME, preserve_folders=True)
+    backup.extract_files(relative_paths_like='Library/SMS/%',
                          output_folder=HOME, preserve_folders=True)
     
 def extract_whatsapp(backup: EncryptedBackup) -> None:
@@ -40,32 +42,29 @@ def export_imessage(format: str, copy_method: str, db_path: str, export_path: st
     args = f'imessage-exporter -f {format} -c {copy_method} -p {db_path} -o {export_path}'.split()
     subprocess.run(args)
 
-# TODO
-def prompt_user():
-    backup_root = '/home/phoenix/Library/Application Support/MobileSync/Backup'
-    hashes = os.listdir(backup_root)
-    df = pd.DataFrame(columns=[
-        'Device Name',
-        'Last Backup Date',
-        'Phone Number',
-        'Product Name',
-        'Unique Identifier'
-    ])
-    for i, hash in enumerate(hashes):
-        backup_path = f'{backup_root}/{hash}'
-        row = get_device_properties(backup_path)
-        print(i, row)
-        df.loc[i] = row
+# # TODO
+# def prompt_user():
+#     backup_root = '/home/phoenix/Library/Application Support/MobileSync/Backup'
+#     ids = os.listdir(backup_root)
+#     df = pd.DataFrame(columns=[
+#         'Device Name',
+#         'Last Backup Date',
+#         'Phone Number',
+#         'Product Name',
+#         'Unique Identifier'
+#     ])
+#     for i, id in enumerate(ids):
+#         backup_path = f'{backup_root}/{id}'
+#         row = get_device_properties(backup_path)
+#         print(i, row)
+#         df.loc[i] = row
 
 def main():
     HOME = os.getenv('HOME')
 
-    # hashes = {
-    #     os.listdir('/mnt/Backup')
-    # }
-    # device_hash = input('Enter device hash: ') # TODO
-    device_hash = '00008030-001C050E0EBB802E'
-    backup_path = f'/mnt/Backup/{device_hash}'
+    # device_id = input('Enter device ID: ') # TODO
+    device_id = '00008030-001C050E0EBB802E'
+    backup_path = f'/mnt/Backup/{device_id}'
     # device_properties = get_device_properties(backup_path)
     # password = input('Enter backup password: ')
     password = 'Fall2024!' or 'Canada!1'
@@ -84,6 +83,10 @@ def main():
 
         # # Extract WhatsApp database and attachments
         # extract_whatsapp(backup)
+
+        # Extract all files
+        # backup.extract_files(relative_paths_like=RelativePathsLike.ALL_FILES, output_folder='ALL_FILES', preserve_folders=True)
+
         
         print('Decryption successful!')
     except Exception as error:
@@ -100,7 +103,7 @@ def main():
 
         args = f'''imessage-exporter -f html -c full
                     -p {HOME}/Library/SMS/sms.db
-                    -o /mnt/Export/{device_hash}'''.split()
+                    -o /mnt/Export/{device_id}'''.split()
         
         subprocess.run(args)
         subprocess.run('sleep infinity'.split())
